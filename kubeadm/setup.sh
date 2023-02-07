aee55f6493824abdf3e019de8bfe2e0cc74f8da9
Zemai <118620383+Zemai@users.noreply.github.com>
2023-02-06 22:42:41 -0500

    Update setup.sh

--- kubeadm/setup.sh
+++ kubeadm/setup.sh
@@ -235,6 +235,8 @@ kubectl get services
 
 ===========================================
 install-kubectl.sh
+
+sudo hostnamectl set-hostname manager
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 curl -LO https://dl.k8s.io/release/v1.26.0/bin/linux/amd64/kubectl
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
@@ -243,6 +245,7 @@ mkdir -p ~/.local/bin
 mv ./kubectl ~/.local/bin/kubectl
 # and then append (or prepend) ~/.local/bin to $PATH
 kubectl version --client
+sudo su - ec2-user
 
 
 
