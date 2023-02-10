1d297bc0e19d1918a5029cb0b5340a6238ea5080
Zemai <118620383+Zemai@users.noreply.github.com>
2023-02-10 13:13:49 -0500

    Update common.sh

--- kubeadm/common.sh
+++ kubeadm/common.sh
@@ -96,5 +96,5 @@ systemctl daemon-reload
 systemctl start kubelet
 systemctl enable kubelet.service
 
-kubeadm join 172.31.89.15:6443 --token 62uyml.ggvuzi8oyuup4sex \
+# kubeadm join 172.31.89.15:6443 --token 62uyml.ggvuzi8oyuup4sex \
         --discovery-token-ca-cert-hash sha256:48ea5958abb0888e6f9a8ec74ef7bd8565606c49f8fdc6fcfa39cbc2d8eda2cb
